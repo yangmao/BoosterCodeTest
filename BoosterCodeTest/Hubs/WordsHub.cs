fe4bad8e16a29ca896ca9ddeac66a3c047ea838f
JessieBai <jessiebai90@gmail.com>
2024-04-30 00:30:01 +1200

    Add project files.

---
+++ BoosterCodeTest/Hubs/WordsHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace BoosterCodeTest.Hubs
+{
+    public class WordsHub:Hub
+    {
+        public async Task Send(string topic, string message)
+        { 
+            await Clients.All.SendAsync(topic,message);
+        }
+    }
+}
